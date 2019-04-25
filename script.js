0720e7a87883894479b878429fbd8cfda8d34048
Nathaniel Kwapo <nathanielkwapo.student@careerdevs.com>
2019-04-25 16:52:44 +0000

    updated script.js to display a description along  with title and  link to wiki page

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 /* global fetch */
 
+const response = ["apple", "banana", "orange"];
+
+
+
 // Click handler
 function handleClick(){
   console.log("Button is clicked");
-  document.getElementById("input").value;
-  displayResults(response);
+  
+  makeAPIcall();
 }
 
+function makeAPIcall(){
 
-const response = ["apple", "banana", "orange"];
-console.log(input);
-const url = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search="+ input +"&limit=10&namespace=0&format=json"
-
-
-
-
-
-fetch(url)
+  const input = document.getElementById("input").value;
+  console.log(input);
+  const url = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search="+ input +"&limit=10&namespace=0&format=json";
+  
+  fetch(url)
   .then(function(response){
-    return response.json()
+    return response.json();
   }).then(function(response){
     console.log(response);
-  })
-
-
+    displayResults(response[1], response[2], response[3]);
+  });
+}
 
 // This function displays results(a array) on the page as a list
-function displayResults(response){
-  
-  const newResponse = response.map(function(item){
+function displayResults(titles, description, links){
+  console.log(links);
+  console.log(description)
+  const newResponse = titles.map(function(item, index){
     console.log(item);
-    return "<li>" + "<a href=#>"+ item + "</a>" + "</li>"
+    
+    return "<li>" + "<a href="+ links[index] +">"+ item + "</a>" + "</li>" + description;
   });
   const joinedResponse = newResponse.join("\n");
   console.log(joinedResponse);
