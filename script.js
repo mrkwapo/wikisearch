5d7d3791fc9536eb36cbe2ff6e4a8b1f0510bfaf
Nathaniel Kwapo <nathanielkwapo.student@careerdevs.com>
2019-04-25 20:20:17 +0000

    I used axios instead of fetch

--- script.js
+++ script.js
@@ -1,43 +1,39 @@
-/* global fetch */
-
-// const response = ["apple", "banana", "orange"];
-
-
+/* global axios */
 
 // Click handler
-function handleClick(){
+function handleClick() {
   console.log("Button is clicked");
-  
+
   makeAPIcall();
 }
 
-function makeAPIcall(){
+function makeAPIcall() {
 
   const input = document.getElementById("input").value;
   console.log(input);
-  const url = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search="+ input +"&limit=10&namespace=0&format=json";
-  
-  fetch(url)
-  .then(function(response){
-    return response.json();
-  }).then(function(response){
-    console.log(response);
-    displayResults(response[1], response[2], response[3]);
-    console.log(response[2]);
-  });
+  const url = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search=" + input + "&limit=10&namespace=0&format=json";
+
+  axios.get(url).then(function(response) {
+      console.log(response);
+      displayResults(response.data[1], response.data[2], response.data[3]);
+      console.log(response[2]);
+    })
+    .catch(function(error) {
+      console.log(error)
+    });
 }
 
 // This function displays results(a array) on the page as a list
-function displayResults(titles, description, links){
+function displayResults(titles, description, links) {
   console.log(links);
-//   console.log(description)
-  const newResponse = titles.map(function(item, index){
+  //   console.log(description)
+  const newResponse = titles.map(function(item, index) {
     console.log(item);
-    
-    return "<li>" + "<a href="+ links[index] +">"+ item + "</a>" + "</li>" + description[index];
+
+    return "<li>" + "<a href=" + links[index] + ">" + item + "</a>" + "</li>" + description[index];
   });
   const joinedResponse = newResponse.join("\n");
   console.log(joinedResponse);
-  
+
   document.getElementById("results").innerHTML = joinedResponse;
 }
